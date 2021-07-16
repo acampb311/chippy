a5655d328a1a1b27f50801da26f2d9c268c67d6e
Adam Campbell <acampb311@outlook.com>
2021-07-15 19:18:15 -0500

    Initial Commit

---
+++ chippy/chippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chippyApp.swift
+//  chippy
+//
+//  Created by Adam Campbell on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct chippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
