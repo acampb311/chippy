52345d9b1f725814087f7f8bdb1f0232b3c00139
Adam Campbell <acampb311@users.noreply.github.com>
2022-03-02 01:28:43 +0000

    .

--- Chip8/Package.swift
+++ Chip8/Package.swift
@@ -12,14 +12,6 @@ let package = Package(
          name: "Chip8",
          targets: ["Chip8"]),
    ],
-//   dependencies: [
-//      // Dependencies declare other packages that this package depends on.
-//      // .package(url: /* package url */, from: "1.0.0"),
-//      .package(
-//         url: "https://github.com/apple/swift-collections.git",
-//         .upToNextMajor(from: "1.0.0") // or `.upToNextMinor
-//      )
-//   ],
    targets: [
       // Targets are the basic building blocks of a package. A target can define a module or a test suite.
       // Targets can depend on other targets in this package, and on products in packages this package depends on.
